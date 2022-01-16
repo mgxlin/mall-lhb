b56235051e55c5f346451af9b32c42c742a4deba
linhb <1113694543@qq.com>
2022-01-16 16:33:03 +0800

    mall-lhb

--- src/main/java/com/lhb/mall/MallLhbApplication.java
+++ src/main/java/com/lhb/mall/MallLhbApplication.java
@@ -1,9 +1,11 @@
 package com.lhb.mall;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan("com.lhb.mall.mbg.mapper")
 public class MallLhbApplication {
 
     public static void main(String[] args) {
